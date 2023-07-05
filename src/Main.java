e9d6bbd348124d98040d7e15629227d8a73c38ea
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-05 18:54:30 +0200

    Calculating square Pi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-//        First program
-public class Main {
+//    Określanie i wyświetlanie zmiennych
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        double pi = 3.14;
+        double squarePi;
+               squarePi = pi * pi;
+        System.out.println("Liczba pi równa się w przybliżeniu" + pi);
+        System.out.println("Kwadrat liczby pi równa się w przybliżeniu" + squarePi);
     }
 }
