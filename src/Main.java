b5a5bb6aaf37cf7a02fea2f8cef774510fc334e7
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-05 19:25:31 +0200

    String formatting

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-//    Określanie i wyświetlanie zmiennych
+//    Wyświetlanie daty
 public class Main{
     public static void main(String[] args) {
-        int radius = 4;
-        double pi = 3.14;
-        double area = radius * radius * pi;
-        double circumference = 2 * pi * radius;
+        int day = 5;
+        String month = "July";
+        int year = 2023;
 
-        System.out.println("Pole koła równa się: " + area);
-        System.out.println("Obwód koła równa się: " + circumference);
+        System.out.println("Today's date is " + day + " " + month + " " + year);
+        System.out.println(String.format("Today's date is %d %s %d and pi=%f", day, month, year, Math.PI));
     }
 }
