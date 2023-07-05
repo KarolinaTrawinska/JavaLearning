b002e904351710d6043597f7502d6f5883537294
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-05 20:26:39 +0200

    User input

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-//    Wyświetlanie daty
-public class Main{
-    public static void main(String[] args) {
-        int day = 5;
-        String month = "July";
-        int year = 2023;
 
-        System.out.println("Today's date is " + day + " " + month + " " + year);
-        System.out.println(String.format("Today's date is %d %s %d and pi=%f", day, month, year, Math.PI));
+////    User input
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Type your name");
+        String name = getString();
+        System.out.println(String.format("Hello %s", name));
+        System.out.println("Type radius");
+
+        int r = getInt();
+        double area = Math.PI * r * r;
+
+        System.out.println("Area is " + area);
     }
+    private static String getString() {
+        String name = new Scanner(System.in).next();
+        return name;
+    }
+
+    private static int getInt() {
+        return new Scanner(System.in).nextInt();
+    }
+
 }
