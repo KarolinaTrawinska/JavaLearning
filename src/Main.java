4bb9735e75018be6c94ec1375532b5a03311e872
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-10 18:56:16 +0200

    Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
-//Continue
-
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        int radius;
+public class Main {
+    public static void main(String[] args) {
 
-        do {
-            System.out.println("Type radius");
-            radius = getInt();
+        System.out.println("Type first side of the rectangle");
+        int a = getInt();
 
-            if (radius <= 0) {
-                System.out.println("Incorrect data, type number bigger than 0");
-                continue;
-            }
-            System.out.println("Area equals" + Math.PI * radius * radius);
-        } while (radius <= 0);
+        System.out.println("Type second side of the rectangle");
+        int b = getInt();
+
+        double area = a * b;
+        if (a > 0 && b > 0) {
+            System.out.println("Rectangle area equals " + area);
+        }
     }
     private static int getInt() {
         return new Scanner(System.in).nextInt();
