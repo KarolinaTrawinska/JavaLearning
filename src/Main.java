2f3122bc470a100a224b1b2f65878243ec863442
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-13 12:25:26 +0200

    Arreys and loops

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-//Tables
+//Arreys
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {-4, 12, 21, 942, -95};
+        boolean found = false;
+        int[] numbers = {-1, 23, 245, 8733, 11243};
+
+        int luckyNumber = 23;
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+            if (numbers[i] == luckyNumber) {
+                break;
+            }
+        }
+            if (found = true) {
+                System.out.println("Lucky number was found");
+            } else {
+                System.out.println("There is no lucky number");
+            }
         }
     }
-}
+
+
+
+
+    
+//Program, który odpowiada na pytanie: czy dana liczba znajduje się w tablicy?
