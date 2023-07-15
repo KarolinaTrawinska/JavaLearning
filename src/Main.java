75b783ae4f39967be067fd69bf4437bc40ce37be
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-15 19:06:59 +0200

    Methods

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,10 @@
-//Arrays
+//Methods
 
 public class Main {
-    public static void main(String[] args) {
-        String[] first = {"I", "like", "music"};
-        String[] second = {"I", "like", "music"};
-
-        boolean differenceFound = false;
-
-        if (first.length != second.length) {
-            differenceFound = true;
-        } else {
-            for (int i = 0; i < first.length; i++) {
-                if (!first[i].equals(second[i])) {
-                    differenceFound = true;
-                    break;
-                }
-            }
-        }
-
-        if (differenceFound) {
-            System.out.println("They are different");
-        } else {
-            System.out.println("They are the same");
-        }
+    public static void main (String[] args) {
+        sayHello();
+    }
+    private static void sayHello() {
+        System.out.println("Hello");
     }
 }
-
-
-
-
-
-
-
-//    Porównanie tablic
