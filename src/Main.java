adb8fcf1a0e4e31434e8f2c49420ec99fa27ff85
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-15 19:59:01 +0200

    Methods - even number

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 //Methods
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        division(10, 0);
-        division(34, 12);
-        division(88, 9);
-    }
-    public static void division(double a, double b) {
-        if (b == 0) {
-            System.out.println("Cannot be divided by 0");
-            return;
-        }
-            System.out.println("Result of division equals: " + (a / b));
+        System.out.println("Type a number");
+        int number = getInt();
+
+        if (isEven(number)) {
+            System.out.println("This number is even");
+        } else {
+            System.out.println("This number is not even");
         }
     }
+    public static int getInt() {
+        return new Scanner(System.in).nextInt();
+    }
+    public static boolean isEven(int number) {
+        return number % 2 ==0;
+    }
+}
