684f0cec2648b06cd67310410dc894ba228cb9d2
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-16 10:23:09 +0200

    Methods - practice

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(lastCharacter("Hello"));
-        System.out.println(lastCharacter("It is a beautiful day"));
+        typeThis(10);
+        typeThis(3.14);
+        typeThis(19, 22);
+        typeThis("Hello");
+        typeThis(new int[]{1, 3, 5});
     }
-    public static char lastCharacter(String s) {
-        return s.charAt(s.length() - 1);
+        public static void typeThis(int number) {
+            System.out.println("Lucky number is: " + number);
+        }
+        public static void typeThis(double x) {
+            System.out.println("Pi equals: " + x);
+        }
+        public static void typeThis(int firstNumber, int secondNumber) {
+            System.out.println("Lucky numbers are: " + firstNumber + "," + secondNumber);
+        }
+        public static void typeThis(String word) {
+            System.out.println("First word is: " + word);
+        }
+        public static void typeThis(int[] table) {
+            for (int i = 0; i < table.length; i++) {
+                System.out.println("Element number " + i + " is: " + table[i]);
+            }
+        }
     }
-}
-
-
-
-//Program zwracający ostatni znak w podanym argumencie stringu
