5cc57aa1e5c65f434c4152be313c60b9884c55ac
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-16 11:10:16 +0200

    First test

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,25 @@ public class Main {
     public static int square(int x) {
         return x * x;
     }
+
     public static void square_positiveNumber_positiveNumbersquare() {
         int result = square(20);
-
-        if (result != 400) {
-            System.out.println("Icorrect result for number 20: " + result);
+        assertEquals(400, result);
         }
-    }
+
     public static void square_negativeNumber_negativeNumbersquare() {
         int result = square(-5);
-
-        if (result != 25) {
-            System.out.println("Incorrect result for number -5: " + result);
+        assertEquals(25, result);
         }
-    }
-    public static void square_numberZero_zero() {
+
+    public static void square_numberZero_zero(){
         int result = square(0);
+        assertEquals(0, result);
+        }
 
-        if (result != 0) {
-            System.out.println("Incorrect result for number 0: " + result);
+   public static void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+        System.out.println("Expected result is different than the actual result. Expected: " + expected + " , actual: " + actual);
+            }
         }
     }
-}
