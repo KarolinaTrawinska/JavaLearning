824eb198f85146c49fc20aa37d49bcb9f807ab5c
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-07-18 18:38:45 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,18 @@
-//First tests
 
 public class Main {
     public static void main(String[] args) {
-        isEven_evenPositivenumber_even();
-        isEven_evenNegativenumber_even();
-        isEven_notEvenpositiveNumber_even();
-        isEven_notEvennegativeNumber_even();
-        isEven_zero_even();
-    }
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
+        Person Karolina = new Person();
+        Karolina.name = "Karolina";
+        Karolina.age = 28;
+        Karolina.isAlive = true;
 
-    public static void isEven_evenPositivenumber_even() {
-        assertEquals(true, isEven(10));
-    }
+        Person Freddie = new Person();
+        Freddie.name = "Freddie";
+        Freddie.age = 77;
+        Freddie.isAlive = false;
 
-    public static void isEven_evenNegativenumber_even() {
-        assertEquals(true, isEven(-10));
-    }
+       Karolina.introduceYourself();
+       Freddie.introduceYourself();
 
-    public static void isEven_notEvenpositiveNumber_even() {
-        assertEquals(false, isEven(13));
-    }
-
-    public static void isEven_notEvennegativeNumber_even() {
-        assertEquals(false, isEven(-27));
-    }
-
-    public static void isEven_zero_even() {
-        assertEquals(true, isEven(0));
-    }
-
-    public static void assertEquals (boolean expected, boolean actual) {
-        if (expected != actual) {
-            System.out.println("Expected result: " + expected + " , actual result: " + actual);
-        }
     }
 }
-
-
-
-
-
-//Czy liczba jest parzysta?
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Person {
+        String name;
+        int age;
+        boolean isAlive;
+
+        public void introduceYourself() {
+            System.out.println("My name is " + name + " and I'm " + age + " years old");
+        }
+}
