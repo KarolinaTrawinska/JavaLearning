a943181407a0c03baebea42dcf8d1928eb58fa5e
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-08-25 08:49:57 +0200

    User counter

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,8 @@
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-        Test square = new Test(new Point[]{
-                new Point(0, 0),
-                new Point(10, 0),
-                new Point(10, 10),
-                new Point(0, 10),
-        });
+        Person p1 = new Person("Karolina", "Trawińska", 28);
+        Person p2 = new Person("Mateusz", "Wiśniewski", 28);
 
-        Test otherSquare = new Test(new Point[] {
-                new Point(2,2),
-                new Point(2, 4),
-                new Point( 4, 2),
-                new Point(4, 4),
-        });
-
-        Test rectangle = new Test(new Point[] {
-                new Point(0, 0),
-                new Point(0, 10),
-                new Point(10, 20),
-                new Point(20, 10),
-        });
-
-        Test otherRectangle = new Test(new Point[] {
-                new Point(0, 0),
-                new Point(0, 10),
-                new Point(10, 20),
-                new Point(20, 10),
-        });
-
-        System.out.println(square.equals(otherSquare));
-        System.out.println(square.equals(rectangle));
-        System.out.println(rectangle.equals(otherSquare));
-        System.out.println(rectangle.equals(otherRectangle));
-        System.out.println(square.equals(null));
+        System.out.println(Person.userCounter);
     }
 }
