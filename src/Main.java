60a6164eb0d2cca97727d89d88a3592a85b4e2ac
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-08-29 12:32:31 +0200

    toString, hashCode and equals

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import computer.Computer;
-import computer.Laptop;
-import computer.PC;
-
 public class Main {
     public static void main(String[] args) {
-        Laptop gamingLaptop = new Laptop("XGame", "Acer", 256, 85);
-        Laptop macbook = new Laptop("MB Pro", "Pro", 256, 99);
+        Person Karolina = new Person("Karolina", "Trawińska", 28);
+        Person Karolina1 = new Person("Karolina", "Trawińska", 28);
 
-        gamingLaptop.sayHello();
+        System.out.println(Karolina.equals(Karolina1));
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private String firstName;
     private String lastName;
@@ -11,25 +13,54 @@ public class Person {
         this.age = age;
         userCounter++;
     }
-    public void setFirstname (String firstName) {
+
+    public void setFirstname(String firstName) {
         this.firstName = firstName;
     }
+
     public String getFirstName() {
         return firstName;
     }
-    public void setLastName (String lastName) {
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getLastName() {
         return lastName;
     }
-    public void setAge (int age) {
+
+    public void setAge(int age) {
         this.age = age;
     }
+
     public int age() {
         return age;
     }
+
     public static int getUserCounter() {
         return userCounter;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
     }
 }
