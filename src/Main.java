16caca7f5bcb4fe406a85ab75a3e4ea952ef0857
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-08-31 16:58:57 +0200

    Stream - allMatch, noneMatch, anyMatch

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
         List<String> names = new ArrayList<>();
         names.add("Karolina");
         names.add("Mateusz");
+        names.add("Filip");
         names.add("Adrian");
         names.add("Kasia");
         names.add("Feliks");
         names.add("Franek");
 
-        names.stream()
+        boolean anyMatch = names.stream()
+                .anyMatch(str -> str.contains("a"));
+        System.out.println(anyMatch);
+
+        boolean allMatch = names.stream()
+                .allMatch(str -> str.length() > 2);
+        System.out.println(allMatch);
+
+        boolean noneMatch = names.stream()
+                .noneMatch(str -> str.endsWith("!"));
+        System.out.println(noneMatch);
+
+        List<String> fNames = names.stream()
                 .filter(str -> str.startsWith("F"))
-                .filter(str -> str.contains("k"))
-                .filter(str -> str.endsWith("s"))
-                .forEach(str -> System.out.println(str));
-        }
+                .collect(Collectors.toList());
+        System.out.println(fNames);
+
     }
+}
