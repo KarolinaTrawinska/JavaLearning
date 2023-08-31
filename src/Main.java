f574046bf5a7c8d06315102d8383fe82d1b4c56f
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-08-31 19:03:04 +0200

    Stream sorting and comparing

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,47 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<String> names = new ArrayList<>();
-        names.add("Karolina");
-        names.add("Mateusz");
-        names.add("Filip");
-        names.add("Adrian");
-        names.add("Kasia");
-        names.add("Feliks");
-        names.add("Franek");
+        List<Person> users = new ArrayList<>();
+        users.add(new Person("Karolina", "Trawinska", 28));
+        users.add(new Person("Mateusz", "Wateusz", 68));
+        users.add(new Person("Kazimiera", "Woźnica", 75));
+        users.add(new Person("Adrian", "Paździoch", 15));
+        users.add(new Person("Anna", "Czarny", 75));
+        users.add(new Person("Anna", "Czarny", 33));
+        users.add(new Person("Anna", "Zielony", 55));
 
-        boolean anyMatch = names.stream()
-                .anyMatch(str -> str.contains("a"));
-        System.out.println(anyMatch);
+        System.out.println(" * USERS * ");
+        for (Person user : users) {
+            System.out.println(user.getFirstName() + ", " + user.getLastName() + ", " + user.getAge());
+        }
 
-        boolean allMatch = names.stream()
-                .allMatch(str -> str.length() > 2);
-        System.out.println(allMatch);
-
-        boolean noneMatch = names.stream()
-                .noneMatch(str -> str.endsWith("!"));
-        System.out.println(noneMatch);
-
-        List<String> fNames = names.stream()
-                .filter(str -> str.startsWith("F"))
+        System.out.println(" * SORTED USERS * ");
+        List<Person> sortedUsers = users.stream()
+                .sorted(Comparator.comparing(user -> user.getFirstName()))
                 .collect(Collectors.toList());
-        System.out.println(fNames);
+        for (Person user : sortedUsers) {
+            System.out.println(user.getFirstName() + ", " + user.getLastName() + ", " + user.getAge());
+        }
 
+        System.out.println(" *  USERS SORTED BY FIRST AND LAST NAME * ");
+        List<Person> extraSortedusers = users.stream()
+                .sorted(Comparator.comparing(Person::getFirstName).thenComparing(Person::getLastName))
+                .collect(Collectors.toList());
+        for (Person user : extraSortedusers) {
+            System.out.println(user.getFirstName() + ", " + user.getLastName() + ", " + user.getAge());
+        }
+
+        System.out.println(" *  USERS SORTED BY AGE, FIRST AND LAST NAME * ");
+        List<Person> sortedByage = users.stream()
+                .sorted(Comparator.comparing(Person::getFirstName).thenComparing(Person::getLastName).thenComparing(Person::getAge))
+                .collect(Collectors.toList());
+        for (Person user : sortedByage) {
+            System.out.println(user.getFirstName() + ", " + user.getLastName() + ", " + user.getAge());
+        }
     }
 }
